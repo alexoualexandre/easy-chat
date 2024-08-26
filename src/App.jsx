752686f436d1cf5cdfa37ce0d18fd4429ca69393
alexandre michiels <alexoualexandre1@gmail.com>
2024-08-26 21:18:32 +0200

    salut

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          chat chat chat chat
+          salut
         </p>
       </div>
       <p className="read-the-docs">
