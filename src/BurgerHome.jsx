41bf6480f48c3413ed7c78cd6df09c9a61db189a
root <root@srv546944.hstgr.cloud>
2024-09-05 19:06:28 +0000

    menu burger presque fini

--- src/BurgerHome.jsx
+++ src/BurgerHome.jsx
@@ -2,7 +2,7 @@ import { MyContext } from "./Context.jsx";
 
 function BurgerHome() {
   const { setBurgerHome } = MyContext();
-  return (
+  return (<>
     <section className="burger-home">
       <button
         className="x"
@@ -13,7 +13,26 @@ function BurgerHome() {
         ×
       </button>
     </section>
-  );
+ <section className="ins-co">
+	<button type="button" className="ins">
+	S&apos;inscrire
+	</button>
+	 <button type="button" className="ins" id="se-connecter">
+        Se connecter
+        </button>
+	<ul className="liste-burger-home">
+	<li className="li-burger-home">
+	<button type="button"
+	 className="return-accueil"
+	onClick={()=>{setBurgerHome(false)}}
+	>
+	Accueil
+	</button>
+	</li>
+	<li className="li-burger-home">Contact</li>
+	</ul>
+      </section>
+  </>);
 }
 
 export default BurgerHome;
