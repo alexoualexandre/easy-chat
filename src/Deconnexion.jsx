e52c769893a3c7dbd3fc161915db148e74a245d8
alexandre michiels <alexoualexandre1@gmail.com>
2024-11-19 23:16:56 +0100

    petit modification dans le composant déconnexion pour mettre a zero present dans la table user

--- src/Deconnexion.jsx
+++ src/Deconnexion.jsx
@@ -3,6 +3,17 @@ import Cookies from "js-cookie";
 function Deconnexion() {
   const env = import.meta.env;
   fetch(
+    `http://${env.VITE_API_URL}:${env.VITE_API_SERVER_PORT}/update-present`,
+
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: 0, m: Cookies.get("auth") }),
+    }
+  ).then((response) => response.json());
+  fetch(
     `http://${env.VITE_API_URL}:${env.VITE_API_SERVER_PORT}/deconnexion/${Cookies.get("auth")}`
   );
   Cookies.remove("auth");
