37ee1a23418537819a8c02b90741000694ab3b6d
alexandre michiels <alexoualexandre1@gmail.com>
2024-12-13 16:08:35 +0100

    modif 2

--- server/api/autoDeconnexion.js
+++ server/api/autoDeconnexion.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 // const { DateTime } = require("luxon");
 
-// const { DateTime } = require('luxon');
+const { DateTime } = require("luxon");
 
 // Obtenir l'heure actuelle
-// const now = DateTime.now();
+const now = DateTime.now();
 
 // Soustraire une heure
 
@@ -16,61 +16,60 @@ function cal() {
     fetch(`http://77.37.51.45:3311/select-all-user`)
       .then((response) => response.json())
       .then((response) => {
-        // for (let i = 0; i < response.length; i++) {
-        // let splitH = parseInt(
-        //   response[i].last_modified.split("T")[1].split(".")[0].split(":")[0],
-        //   10
-        // );
-        // let heure = splitH;
-        // let splitH = parseInt(
-        //   response[i].last_modified.split("T")[1].split(".")[0].split(":")[0],
-        //   10
-        // );
+        for (let i = 0; i < response.length; i++) {
+          // let splitH = parseInt(
+          //   response[i].last_modified.split("T")[1].split(".")[0].split(":")[0],
+          //   10
+          // );
+          // let heure = splitH;
+          // let splitH = parseInt(
+          //   response[i].last_modified.split("T")[1].split(".")[0].split(":")[0],
+          //   10
+          // );
 
-        // let oneMinuteAgo = now.minus({ minutes: 1 });
-        // let split = response[i].last_modified.split("T")[1].split(".")[0];
+          let oneMinuteAgo = now.minus({ minutes: 1 });
+          let split = response[i].last_modified.split("T")[1].split(".")[0];
 
-        // console.log(oneMinuteAgo.toFormat("HH:mm:ss"))
-        console.log(response[0].last_modified.split("T")[1].split(".")[0]);
+          // console.log(oneMinuteAgo.toFormat("HH:mm:ss"))
+          // console.log(response[0].last_modified.split("T")[1].split(".")[0] === "15:57:57");
 
-        // .split(".")[0].split(":")[1],
+          // .split(".")[0].split(":")[1],
 
-        // let heure = splitH;
-        // let minute = splitM;
-        // let now = DateTime.now();
-        // let nowM = parseInt(now.minute, 10);
+          // let heure = splitH;
+          // let minute = splitM;
+          // let now = DateTime.now();
+          // let nowM = parseInt(now.minute, 10);
 
-        // if (split === oneMinuteAgo.toFormat("HH:mm:ss")) {
-        //   fetch(
-        //     `http://localhost:3311/auto-deco`,
+          if (split === oneMinuteAgo.toFormat("HH:mm:ss")) {
+            fetch(
+              `http://77.37.51.45:3311/auto-deco`,
 
-        //     {
-        //       method: "PUT",
-        //       headers: {
-        //         "Content-Type": "application/json",
-        //       },
-        //       body: JSON.stringify({
-        //         id: response[i].id,
-        //       }),
-        //     }
-        //   );
-        //   fetch(
-        //     `http://localhost:3311/update-present`,
+              {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  id: response[i].id,
+                }),
+              }
+            );
+            fetch(
+              `http://77.37.51.45:3311/update-present`,
 
-        //     {
-        //       method: "PUT",
-        //       headers: {
-        //         "Content-Type": "application/json",
-        //       },
-        //       body: JSON.stringify({
-        //         user: 0,
-        //         m: response[i].id,
-        //       }),
-        //     }
-        //   );
-        // }
-
-        // }
+              {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  user: 0,
+                  m: response[i].id,
+                }),
+              }
+            );
+          }
+        }
       })
       .then((response) => response.json());
   }, 330);
