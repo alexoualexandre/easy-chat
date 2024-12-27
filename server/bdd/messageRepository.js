cef93993d40246ab0aba7d3b4ad4741bbc820635
alexandre michiels <alexoualexandre1@gmail.com>
2024-12-28 00:20:36 +0100

    correction message 5

--- server/bdd/messageRepository.js
+++ server/bdd/messageRepository.js
@@ -26,8 +26,12 @@ class Message {
 
   async getMessage(data) {
     const [result] = await connection.query(
-      "SELECT dest,exp,message,sex,new FROM message JOIN user ON message.exp = user.id WHERE addition = ? AND dest = ? OR exp = ?",
-      [parseInt(data.id, 10) + parseInt(data.auth, 10),parseInt(data.id, 10)]
+      "SELECT dest,exp,message,sex,new FROM message JOIN user ON message.exp = user.id WHERE addition = ? AND (dest = ? OR exp = ?)",
+      [
+        parseInt(data.id, 10) + parseInt(data.auth, 10),
+        parseInt(data.id, 10),
+        parseInt(data.id, 10),
+      ]
     );
     return result;
   }
