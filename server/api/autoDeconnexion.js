44feded96218611f045a40aec7a050ee384b8720
alexandre michiels <alexoualexandre1@gmail.com>
2025-01-26 14:53:05 +0100

    je revient sur http

--- server/api/autoDeconnexion.js
+++ server/api/autoDeconnexion.js
@@ -4,7 +4,7 @@ const { DateTime } = require("luxon");
 
 function cal() {
   const interval = setInterval(() => {
-    fetch(`https://easy-chat.org:3311/select-all-user`)
+    fetch(`http://easy-chat.org:3311/select-all-user`)
       .then((response) => response.json())
       .then((response) => {
         for (let i = 0; i < response.length; i++) {
@@ -26,7 +26,7 @@ function cal() {
             parseInt(oneMinuteAgo.second, 10) == second
           ) {
             fetch(
-              `https://easy-chat.org:3311/auto-deco`,
+              `http://easy-chat.org:3311/auto-deco`,
 
               {
                 method: "PUT",
@@ -39,7 +39,7 @@ function cal() {
               }
             );
             fetch(
-              `https://easy-chat.org:3311/update-present`,
+              `http://easy-chat.org:3311/update-present`,
 
               {
                 method: "PUT",
