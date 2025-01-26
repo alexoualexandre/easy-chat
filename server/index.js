99ac25b78e3f89994df6d264d8cf355d9967ae73
alexandre michiels <alexoualexandre1@gmail.com>
2025-01-26 14:12:09 +0100

    ca

--- server/index.js
+++ server/index.js
@@ -10,18 +10,18 @@ const app = express();
 
 app.use(express.json());
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     const whitelist = ["http://localhost:5173", "https://easy-chat.org:80"];
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
+const corsOptions = {
+  origin: function (origin, callback) {
+    const whitelist = ["http://localhost:5173", "https://easy-chat.org"];
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(cors());
 
 app.use("/upload", express.static(path.join(__dirname, "/upload")));
