48ac0c6c24d0d44cfb9b5795df73dab5c7347493
alexandre michiels <alexoualexandre1@gmail.com>
2025-06-08 20:22:09 +0200

    voir video fini

--- src/Video.jsx
+++ src/Video.jsx
@@ -23,6 +23,8 @@ function Video() {
       });
   }, []);
 
+  // console.log(video[0].vid);
+
   return (
     <div className="block-this-video">
       <Link to={`/home?dest=${params.get("dest")}`}>
@@ -34,12 +36,32 @@ function Video() {
           ×
         </button>
       </Link>
-      {video && (
+      {video && video[0].vid !== " " ? (
         <video
           controls
           src={`${env.VITE_API_HTTP}://${env.VITE_API_URL}:${env.VITE_API_SERVER_PORT}/upload/${video[0].vid}`}
           className="video-user"
         />
+      ) : (
+        <>
+          <img
+            src="istockphoto-1316006857-612x612.png"
+            alt="oups"
+            className="oups"
+          />
+          <h3
+            style={{
+              color: "white",
+              width: "100%",
+              height: "20%",
+              position: "absolute",
+              top: "70%",
+              textAlign: "center",
+            }}
+          >
+            Cet utilisateur n&apos;as pas de vidéo
+          </h3>
+        </>
       )}
     </div>
   );
